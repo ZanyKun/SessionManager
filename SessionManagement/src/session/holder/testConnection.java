2066536e7e0da950c1005795b4953aadf42c0111
ZanyKun <nunez.kev59@gmail.com>
2020-04-01 22:21:20 -0500

    Updated Session Management Files

--- SessionManagement/src/session/holder/testConnection.java
+++ SessionManagement/src/session/holder/testConnection.java
@@ -1,5 +1,24 @@
 package session.holder;
 
-public class testConnection {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
 
+import session.dao.DbUtil;
+
+public class testConnection {
+	public static void main(String... args) {
+		String insertQuery = "insert into BillDetails values(seq_bill_num.nextval, ?, ?, ?, ?, " + LocalDateTime.now() + ")";
+		String selectQuery = "select * from Consumers";
+		try(Connection c = DbUtil.getConnection();
+			PreparedStatement s = c.prepareStatement(insertQuery);
+			PreparedStatement s1 = c.prepareStatement(selectQuery);){
+			System.out.println(s1);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
